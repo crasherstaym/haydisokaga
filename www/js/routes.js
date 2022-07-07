7cacbba49d914b4ee0770fd0e2d7dda49f54d751
Y.Emre USTUNER <webmasyer.yeu@gmail.com>
2022-07-07 13:47:52 +0300

    kayit ekranı güncellendi

--- www/js/routes.js
+++ www/js/routes.js
@@ -3,5 +3,13 @@ var routes = [
     "path": "/",
     "url": "./index.html",
     "name": "index"
+  },
+  {
+    "path": "/kayit",
+    "url": "./kayit.html"
+  },
+  {
+    "path": "/kayit",
+    "url": "kayit.html"
   }
 ]
